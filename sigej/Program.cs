7f6cca3590db32f4272a5c6f310eea9c98a50d43
Matheus Castelo <matheuscastelo.tech@gmail.com>
2025-12-04 16:48:33 -0300

    initial commit

---
+++ sigej/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace sigej;
+
+static class Program
+{
+    private const string ConnectionString = "Host=localhost;Username=sigej_admin;Password=admin123;Database=sigej_db";
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--- Teste de Conexão SIGEJ ---");
+    }
+}
